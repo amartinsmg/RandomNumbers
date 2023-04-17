c20cecb30e80b889cc99e47d253f36e819e67dc6
andre <andremindurimg@gmail.com>
2023-04-17 19:53:56 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/**
+  Configuration object for Tailwind CSS.
+    @property content - An array of file globs used for content extraction.
+    @property theme - An object containing the theme configuration.
+    @property theme.extend - An object containing the custom theme configuration to be added to the default Tailwind CSS theme.
+    @property plugins - An array of plugins to be used with Tailwind CSS.
+*/
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
