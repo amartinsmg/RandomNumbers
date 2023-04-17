3192d5af7a2f2e2250839bbb450d4d304ed449f4
andre <andremindurimg@gmail.com>
2023-04-17 19:53:25 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
+// Configuration file for PostCSS plugins
+
 const tailwindcss = require("tailwindcss");
 
+/**
+  Configuration object for PostCSS plugins
+*/
+
 module.exports = {
+/**
+  An array of PostCSS plugins to be used
+*/
+
   plugins: ["postcss-preset-env", tailwindcss],
 };
