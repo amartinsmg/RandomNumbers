c12ed6eb3aec8cbb6bfd57b9eb175492e70505a1
andre <andremindurimg@gmail.com>
2023-06-27 19:24:29 -0300

    Update random_numbers.js

--- src/random_numbers.js
+++ src/random_numbers.js
@@ -5,7 +5,7 @@
   duplicated or not, if decimal numbers are allowed and if the array should be sorted.
     @param min - The minimum value of the range. Must be a number.
     @param max - The maximum value of the range. Must be a number greater than min.
-    @param length - Optional parameter to specify the length of the array. Must be a number.
+    @param length - The length of the array. Must be a number.
     @param duplication - Optional parameter to specify whether the array can have duplicated values. Must be a boolean.
     @param decimal - Optional parameter to specify whether decimal numbers are allowed. Must be a boolean.
     @param sort - Optional parameter to specify whether the array should be sorted. Must be a boolean.
