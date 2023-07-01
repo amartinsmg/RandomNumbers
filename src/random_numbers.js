f17913bf40c35e7ed58b6b7624b1eb33e84169c2
andre <andremindurimg@gmail.com>
2023-07-01 18:08:51 -0300

    Update random_numbers.js

--- src/random_numbers.js
+++ src/random_numbers.js
@@ -1,15 +1,15 @@
 /**
-  Generates an array of random numbers within a range with optional parameters.
+  @brief Generates an array of random numbers within a range with optional parameters.
   The function generates an array of random numbers between a minimum and maximum value,
   with optional parameters to specify the length of the array, whether the numbers can be
   duplicated or not, if decimal numbers are allowed and if the array should be sorted.
-    @param min - The minimum value of the range. Must be a number.
-    @param max - The maximum value of the range. Must be a number greater than min.
-    @param length - The length of the array. Must be a number.
-    @param duplication - Optional parameter to specify whether the array can have duplicated values. Must be a boolean.
-    @param decimal - Optional parameter to specify whether decimal numbers are allowed. Must be a boolean.
-    @param sort - Optional parameter to specify whether the array should be sorted. Must be a boolean.
-    @return - An array of random numbers within the specified range and with the specified properties.
+  @param min - The minimum value of the range. Must be a number.
+  @param max - The maximum value of the range. Must be a number greater than min.
+  @param length - The length of the array. Must be a number.
+  @param duplication - Optional parameter to specify whether the array can have duplicated values. Must be a boolean.
+  @param decimal - Optional parameter to specify whether decimal numbers are allowed. Must be a boolean.
+  @param sort - Optional parameter to specify whether the array should be sorted. Must be a boolean.
+  @return - An array of random numbers within the specified range and with the specified properties.
 */
 
 function randomNumbers(min, max, length, duplication, decimal, sort) {
