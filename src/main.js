ee8bd693e58d22f95f9030bd55b3bc5743b463bc
André Martins <andremindurimg@gmail.com>
2026-03-07 15:15:35 -0300

    Update paths in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./main.css";
-import randomNumbers from "./random_numbers";
-import roundTo from "./round_to";
+import randomNumbers from "./js/random_numbers";
+import roundTo from "./js/round_to";
 
 // Handles the form submit event to generate random numbers based on user input.
 
